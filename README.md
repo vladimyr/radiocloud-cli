a3ce75e9afcc7eb1be2f9894892dd41e6bbf0bb7
Dario Vladovic <d.vladimyr@gmail.com>
2018-05-25 19:18:02 +0200

    Implement radiocloud CLI
    - fetching stations from github repo
    - playing using QuickTime player & applescript :tada:

---
+++ README.md
@@ -0,0 +1,6 @@
+# radiocloud-cli [![npm package version](https://img.shields.io/npm/v/radiocloud-cli.svg)](https://npm.im/radiocloud-cli) [![github license](https://img.shields.io/github/license/vladimyr/radiocloud-cli.svg)](https://github.com/vladimyr/radiocloud-cli/blob/master/LICENSE) [![js-semistandard-style](https://img.shields.io/badge/code%20style-semistandard-brightgreen.svg)](https://github.com/Flet/semistandard)
+
+>Play [radiocloud](https://radiocloud.surge.sh) radio stations from your terminal
+
+:construction:
+
---
+++ cli.js
@@ -0,0 +1,57 @@
+#!/usr/bin/env node
+
+'use strict';
+
+const diacritics = require('diacritics');
+const fuzzysearch = require('fuzzysearch');
+const got = require('got');
+const inquirer = require('inquirer');
+const pkg = require('./package.json');
+const run = require('run-applescript');
+inquirer.registerPrompt('autocomplete', require('inquirer-autocomplete-prompt'));
+
+const fetchStations = async (url = pkg.config.stationsUrl) => {
+  const { body: stations } = await got(url, { json: true });
+  return stations;
+};
+const removeDiacritics = str => diacritics.remove(str.replace(/đ/g, 'dj'));
+const normalize = str => removeDiacritics(str.toLowerCase().trim());
+
+const playStream = (streamUrl, singleInstance = true) => run(`
+tell application "QuickTime Player"
+  ${singleInstance && `
+    if it is running and (exists document 1) then
+      close document 1
+    end if
+    activate
+  `}
+  open URL "${streamUrl}"
+end tell`);
+
+program().catch(err => { throw err; });
+
+async function program() {
+  const stations = await fetchStations();
+  const station = await selectStation(stations);
+  return playStream(station.url);
+}
+
+async function selectStation(stations) {
+  stations = stations.map(station => {
+    station.value = station;
+    station.normalizedName = normalize(station.name);
+    return station;
+  });
+  const { station } = await inquirer.prompt([{
+    type: 'autocomplete',
+    name: 'station',
+    message: 'Select station',
+    pageSize: 10,
+    source: async (_, input) => {
+      if (!input) return stations;
+      const needle = normalize(input);
+      return stations.filter(station => fuzzysearch(needle, station.normalizedName));
+    }
+  }]);
+  return station;
+}
